e6ec8faf4406e9941acee0123bcf1581dc67de82
Raphael Pierzina <raphael@hackebrot.de>
2017-03-02 16:14:02 +0000

    Implement simple cmd for loading the env var

---
+++ librariesio/cmd/librariesio/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func loadFromEnv(keys ...string) (map[string]string, error) {
+	env := make(map[string]string)
+
+	for _, key := range keys {
+		v := os.Getenv(key)
+		if v == "" {
+			return nil, fmt.Errorf("environment variable %q is required", key)
+		}
+		env[key] = v
+	}
+
+	return env, nil
+}
+
+func main() {
+	env, err := loadFromEnv("LIBRARIESIO_API_KEY")
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Fprintf(os.Stdout, "%v\n", env)
+}
