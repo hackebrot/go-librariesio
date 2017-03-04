33362911d547548214a34ac0be71256700a31fb4
Raphael Pierzina <raphael@hackebrot.de>
2017-03-04 02:52:20 +0000

    Add other fields to Project struct

--- librariesio/projects.go
+++ librariesio/projects.go
@@ -3,11 +3,33 @@ package librariesio
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
-// Project that holds a name field
+// Project represents a project on libraries.io
 type Project struct {
-	Name string `json:"name"`
+	Description              string    `json:"description,omitempty"`
+	Forks                    int       `json:"forks,omitempty"`
+	Homepage                 string    `json:"homepage,omitempty"`
+	Keywords                 []string  `json:"keywords,omitempty"`
+	Language                 string    `json:"language,omitempty"`
+	LatestReleaseNumber      string    `json:"latest_release_number,omitempty"`
+	LatestReleasePublishedAt time.Time `json:"latest_release_published_at,omitempty"`
+	LatestStableRelease      Release   `json:"latest_stable_release,omitempty"`
+	Name                     string    `json:"name,omitempty"`
+	NormalizedLicenses       []string  `json:"normalized_licenses,omitempty"`
+	PackageManagerURL        string    `json:"package_manager_url,omitempty"`
+	Platform                 string    `json:"platform,omitempty"`
+	Rank                     int       `json:"rank,omitempty"`
+	Stars                    int       `json:"stars,omitempty"`
+	Status                   string    `json:"status,omitempty"`
+	Versions                 []Release `json:"versions,omitempty"`
+}
+
+// Release represents a release of the project
+type Release struct {
+	Number      string    `json:"number,omitempty"`
+	PublishedAt time.Time `json:"published_at,omitempty"`
 }
 
 // GetProject returns information about a project and it's versions.
