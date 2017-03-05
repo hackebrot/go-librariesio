0a625aa2a0acbb07a95fd63d221f4e81589a4818
Raphael Pierzina <raphael@hackebrot.de>
2017-03-05 01:38:50 +0000

    Change Project fields to pointers

--- librariesio/projects.go
+++ librariesio/projects.go
@@ -9,22 +9,22 @@ import (
 
 // Project represents a project on libraries.io
 type Project struct {
-	Description              string    `json:"description,omitempty"`
-	Forks                    int       `json:"forks,omitempty"`
-	Homepage                 string    `json:"homepage,omitempty"`
-	Keywords                 []string  `json:"keywords,omitempty"`
-	Language                 string    `json:"language,omitempty"`
-	LatestReleaseNumber      string    `json:"latest_release_number,omitempty"`
-	LatestReleasePublishedAt time.Time `json:"latest_release_published_at,omitempty"`
-	LatestStableRelease      Release   `json:"latest_stable_release,omitempty"`
-	Name                     string    `json:"name,omitempty"`
-	NormalizedLicenses       []string  `json:"normalized_licenses,omitempty"`
-	PackageManagerURL        string    `json:"package_manager_url,omitempty"`
-	Platform                 string    `json:"platform,omitempty"`
-	Rank                     int       `json:"rank,omitempty"`
-	Stars                    int       `json:"stars,omitempty"`
-	Status                   string    `json:"status,omitempty"`
-	Versions                 []Release `json:"versions,omitempty"`
+	Description              *string    `json:"description,omitempty"`
+	Forks                    *int       `json:"forks,omitempty"`
+	Homepage                 *string    `json:"homepage,omitempty"`
+	Keywords                 []*string  `json:"keywords,omitempty"`
+	Language                 *string    `json:"language,omitempty"`
+	LatestReleaseNumber      *string    `json:"latest_release_number,omitempty"`
+	LatestReleasePublishedAt *time.Time `json:"latest_release_published_at,omitempty"`
+	LatestStableRelease      *Release   `json:"latest_stable_release,omitempty"`
+	Name                     *string    `json:"name,omitempty"`
+	NormalizedLicenses       []*string  `json:"normalized_licenses,omitempty"`
+	PackageManagerURL        *string    `json:"package_manager_url,omitempty"`
+	Platform                 *string    `json:"platform,omitempty"`
+	Rank                     *int       `json:"rank,omitempty"`
+	Stars                    *int       `json:"stars,omitempty"`
+	Status                   *string    `json:"status,omitempty"`
+	Versions                 []*Release `json:"versions,omitempty"`
 }
 
 // Release represents a release of the project
