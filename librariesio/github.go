bde402eb58c313862dfb3753ea6806eaa1e7e36f
Raphael Pierzina <raphael@hackebrot.de>
2017-04-27 23:06:22 +0200

    Implement User endpoint

--- librariesio/github.go
+++ librariesio/github.go
@@ -1,6 +1,11 @@
 package librariesio
 
-import "time"
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+)
 
 // User represents a user on libraries.io
 type User struct {
@@ -22,4 +27,28 @@ type User struct {
 	Following    *int       `json:"following,omitempty"`
 	HostType     *string    `json:"host_type,omitempty"`
 	GitHubID     *int       `json:"github_id,omitempty"`
+}
+
+// User returns information for a given user or organization
+//
+// GET https://libraries.io/api/github/hackebrot
+//
+// login is a user or organization on GitHub
+func (c *Client) User(ctx context.Context, login string) (*User, *http.Response, error) {
+	urlStr := fmt.Sprintf("github/%v", login)
+
+	request, err := c.NewRequest("GET", urlStr, nil)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	user := new(User)
+
+	response, err := c.Do(ctx, request, user)
+	if err != nil {
+		return nil, response, err
+	}
+
+	return user, response, nil
 }
