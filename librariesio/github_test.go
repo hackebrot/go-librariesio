b87e120bb45d4d7b11b83bcbee238c9d125d8952
Raphael Pierzina <raphael@hackebrot.de>
2017-05-28 00:39:00 +0200

    Implement unit test for client.UserRepositories()

--- librariesio/github_test.go
+++ librariesio/github_test.go
@@ -7,6 +7,7 @@ import (
 	"reflect"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/hackebrot/go-repr/repr"
 )
@@ -107,5 +108,70 @@ func TestUserProjects(t *testing.T) {
 
 	if !reflect.DeepEqual(projects, want) {
 		t.Errorf("\nExpected %v\nGot %v", repr.Repr(want), repr.Repr(projects))
+	}
+}
+
+func TestUserRepositories(t *testing.T) {
+	server, mux, url := startNewServer()
+	client := NewClient(APIKey)
+	client.BaseURL = url
+	defer server.Close()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if method := "GET"; method != r.Method {
+			t.Errorf("expected HTTP %v request, got %v", method, r.Method)
+		}
+
+		if url := r.URL.String(); !strings.Contains(url, "/github/hackebrot/repositories") {
+			t.Errorf("unexpected URL, got %v", url)
+		}
+
+		fmt.Fprintf(w, `[
+			{
+				"full_name": "hackebrot/go-repr",
+				"default_branch": "master",
+				"language": "Go",
+				"keywords": ["go", "golang", "helper"],
+				"license": "MIT",
+				"pushed_at": "2017-03-18T23:55:35.000Z",
+				"private": false
+			},
+			{
+				"full_name": "hackebrot/dotfiles",
+				"language": "Shell",
+				"pushed_at": "2017-01-22T20:57:47.000Z"
+			}
+		]`)
+	})
+
+	repos, _, err := client.UserRepositories(context.Background(), "hackebrot")
+
+	if err != nil {
+		t.Fatalf("UserRepositories returned unexpected error: %v", err)
+	}
+
+	want := []*Repository{
+		{
+			FullName:      String("hackebrot/go-repr"),
+			DefaultBranch: String("master"),
+			Language:      String("Go"),
+			Keywords: []*string{
+				String("go"),
+				String("golang"),
+				String("helper"),
+			},
+			License:  String("MIT"),
+			PushedAt: Time(time.Date(int(2017), time.March, int(18), int(23), int(55), int(35), int(0), time.UTC)),
+			Private:  Bool(false),
+		},
+		{
+			FullName: String("hackebrot/dotfiles"),
+			Language: String("Shell"),
+			PushedAt: Time(time.Date(int(2017), time.January, int(22), int(20), int(57), int(47), int(0), time.UTC)),
+		},
+	}
+
+	if !reflect.DeepEqual(repos, want) {
+		t.Errorf("\nExpected %v\nGot %v", repr.Repr(want), repr.Repr(repos))
 	}
 }
