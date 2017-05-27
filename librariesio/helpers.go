50efb0bb68b6823540bd7f4b9ead9d40daec8787
Raphael Pierzina <raphael@hackebrot.de>
2017-05-27 22:39:40 +0200

    Add Time() helper

--- librariesio/helpers.go
+++ librariesio/helpers.go
@@ -1,5 +1,7 @@
 package librariesio
 
+import "time"
+
 // Bool returns a *bool for the given value
 func Bool(b bool) *bool {
 	return &b
@@ -13,4 +15,9 @@ func Int(i int) *int {
 // String returns a *string for the given value
 func String(s string) *string {
 	return &s
+}
+
+// Time returns a *time.Time for the given value
+func Time(t time.Time) *time.Time {
+	return &t
 }
